04e3e089b88a0f68a07f3f491dae9f37aa811cd0
AlbertoTrujillo0221 <57775191+AlbertoTrujillo0221@users.noreply.github.com>
2024-09-26 20:13:47 -0500

    FirstCommit

---
+++ KafkaTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    while (true)
+    {
+        var response = await producer.ProduceAsync(
+                        "LiteThinkingTestz",
+                        new Message<Null, string>
+                        {
+                            Value = JsonConvert.SerializeObject(new Order("1234", 10000, "Cali"))
+                        });
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+
+public record Order(string OrderNumber, int Price, string City);
